5a01a71e4230c3f36b52a8c903c5b1fe76bd69b5
Jared <jareddj34@gmail.com>
2023-07-19 22:43:36 -0700

    second commit

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -3,10 +3,15 @@
 import { User } from "next-auth";
 import {
     DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
+} from "@/components/ui/DropdownMenu";
 import { FC } from "react";
 import UserAvatar from "./UserAvatar";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
 
 interface UserAccountNavProps {
     user: Pick<User, "name" | "image" | "email">;
@@ -24,6 +29,49 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
                     }}
                 />
             </DropdownMenuTrigger>
+
+            <DropdownMenuContent className="bg-white" align="end">
+                <div className="flex items-center justify-start gap-2 p-2">
+                    <div className="flex flex-col space-y-1 leading-none">
+                        {user.name && (
+                            <p className="font-medium">{user.name}</p>
+                        )}
+                        {user.email && (
+                            <p className="w-[200px] truncate text-sm text-zinc-700">
+                                {user.email}
+                            </p>
+                        )}
+                    </div>
+                </div>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem asChild>
+                    <Link href="/">Feed</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem asChild>
+                    <Link href="/r/create">Create community</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem asChild>
+                    <Link href="/settings">Settings</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem
+                    onSelect={(event) => {
+                        event.preventDefault();
+                        signOut({
+                            callbackUrl: `${window.location.origin}/sign-in`,
+                        });
+                    }}
+                    className="cursor-pointer"
+                >
+                    Sign out
+                </DropdownMenuItem>
+            </DropdownMenuContent>
         </DropdownMenu>
     );
 };
